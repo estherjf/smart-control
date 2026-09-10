d38c04a693eecabe220bbacf93630a83f45ba0d8
esthejf <esthervieira9876@gmail.com>
2026-09-10 17:06:34 -0300

    atualizei a responsividade, e melhorei a lista de dispositivos dos cômodos.

--- assets/js/pagina-comodo.js
+++ assets/js/pagina-comodo.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const listaEl = document.getElementById("lista-dispositivos");
+    const conteudo = document.querySelector('.conteudo');
+    
     if (!listaEl) return;
 
     const comodo = document.body.dataset.comodo;
     const dispositivos = porComodo(comodo);
 
     listaEl.innerHTML = "";
-
-    dispositivos.forEach(d => {
-        const item = document.createElement("div");
-        item.className = "aparelho";
-        item.textContent = d.nome;
-        listaEl.appendChild(item);
-    });
+    if(dispositivos.length > 0){
+        dispositivos.forEach(d => {
+            const item = document.createElement("div");
+            item.className = "aparelho";
+            item.textContent = d.nome;
+            listaEl.appendChild(item);
+        });
+    } else {
+        conteudo.innerHTML = "<h1 style='text-align:center'>Não há dispositivos cadastrados!</h1>"
+    }
 });
